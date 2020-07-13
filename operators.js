90710cb1c772324096223fe1af024fe5f65ca2c0
Weil Lee <cugblw2014@outlook.com>
2020-07-13 23:43:40 +0800

    operators

---
+++ operators.js
@@ -0,0 +1,36 @@
+// Categories of Operators
+
+// Assignment =
+// Arithmetic + - * / %
+
+// var m = 10 % 3;
+// console.log(m);
+
+/*
+// Increment / Decrement ++ --
+var a = 1;
+a++;
+// console.log(a++);
+console.log(++a);
+console.log(a);
+*/
+
+// String '' +
+
+// Precedence
+// var b = 1 + 2 * 3;
+// console.log(b);
+// var b = (1 + 2) * 3;
+// console.log(b);
+
+// Function invocation operators ()
+// console.log('')
+
+// Logical and: && or: ||
+
+// Member accessor operator .
+// console.log()
+
+// Code block operator { }
+
+// Array element access operator
