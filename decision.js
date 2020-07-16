295440e86188fb63dc31573f106da8f1030cdc3e
Weil Lee <cugblw2014@outlook.com>
2020-07-17 00:13:23 +0800

    decision

---
+++ decision.js
@@ -0,0 +1,36 @@
+/*
+let counter = 3;
+
+if (counter == 4) {
+  console.log("Count is 4");
+} else if (counter > 4) {
+  console.log("Count is greater than 4");
+} else if (counter < 4) {
+  console.log('Counter is less than 4');
+} else {
+  console.log('Count is not 4');
+}
+*/
+
+/*
+let hero = 'superman';
+
+switch (hero.toLowerCase()){
+  case 'superman':
+    console.log('super strength');
+    console.log('x-ray vision');
+    break;
+  case 'batman':
+    console.log('intelligence');
+    console.log('fighting skills');
+    break;
+  default:
+    console.log('member of JLA');
+}
+*/
+
+// ternary operator
+let a = 1, b= '1';
+let result = (a === b)?'equal':'inequal';
+console.log(result);
+// console.log((a == b)?'equal':'inequal');
--- desicion.js
+++
@@ -1,1 +0,0 @@
-if 'bob' is in 
