c39c832156aa5f94bf7d3fdfd5bbaf377de9cef9
Weil Lee <cugblw2014@outlook.com>
2020-07-23 10:17:49 +0800

    this keyword

--- this-keyword.js
+++ this-keyword.js
@@ -20,15 +20,50 @@ console.log(second() === global);
 console.log(second() === undefined);
 */
 
-let myObject = {
+// this depends on How a function is called.
+
+// An object can be passed as the first argument to call 
+// or apply and this will be bound to it.
+
+/* let myObject = {
     value: 'My Object'
 };
 
-// value is set on the global object
+// This property is set on the global object
 global.value = 'Global object';
 
-function third() {
-    return this.value;
+function third(name) {
+    // Returns something different depending on how 
+    // we call this method:  
+    return this.value + name;
 }
 
 console.log(third());
+
+// Both call and apply allow you to explicitly set
+// what you want to represent 'this'. The difference
+// is in how additional arguments to the function
+// are sent.
+
+console.log(third.call(myObject,'bob'));
+console.log(third.apply(myObject,['bob'])); 
+*/
+
+function fifth (){
+    console.log(this.firstName + ' ' + this.lastName);
+}
+
+let customer1 = {
+    firstName: 'bob',
+    lastName: 'tabor',
+    print:fifth
+}
+
+let customer2 = {
+    firstName: 'richard',
+    lastName: 'boughton',
+    print:fifth
+}
+
+customer2.print();
+customer1.print();
